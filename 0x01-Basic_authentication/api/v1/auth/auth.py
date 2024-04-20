72694e1363bdee6c94bddf71a1b8b25334f23e1c
ondanje <cyrilondanje@gmail.com>
2024-04-20 16:46:12 +0300

    authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,7 +31,9 @@ class Auth:
         def authorization_header(self, request=None)
         -> str: that returns None for now
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """
