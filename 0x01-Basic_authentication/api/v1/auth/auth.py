ae4c7caafef5e58f63253d5b44ad437cf12a7c9b
ondanje <cyrilondanje@gmail.com>
2024-04-20 13:44:59 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,15 @@ class Auth:
         excluded_paths: List[str]) -> bool: that returns
         False - path and excluded_paths for now
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
