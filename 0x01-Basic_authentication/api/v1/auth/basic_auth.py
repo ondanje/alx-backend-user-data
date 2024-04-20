3e677de54b3dc94d5657d6f5e55402fe9f34dcc3
ondanje <cyrilondanje@gmail.com>
2024-04-20 17:41:42 +0300

    basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic auth class to manage authentication
+"""
+from auth import Auth
+from flask import request
+from typing import List, TypeVar
+
+
+class BasicAuth(Auth):
+    """
+    a class BasicAuth that inherits from Auth
+    """
+    pass
