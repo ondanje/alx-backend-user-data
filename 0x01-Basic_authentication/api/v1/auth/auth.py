217669404b65f066e5eead7fcfda28f77e881739
ondanje <cyrilondanje@gmail.com>
2024-04-20 12:45:48 +0300

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Auth class to manage authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        public method def require_auth(self, path: str,
+        excluded_paths: List[str]) -> bool: that returns
+        False - path and excluded_paths for now
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        def authorization_header(self, request=None)
+        -> str: that returns None for now
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+        """
+        Get the current user based on the request.
+        Returns None for now, will be updated later.
+        """
+        return None
